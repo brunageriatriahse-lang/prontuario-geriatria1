bec0800370f7cc6dc18c109822f04fc77a51c8a7
brunageriatriahse-lang <brunageriatriahse@gmail.com>
2026-06-23 10:52:30 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://script.google.com/macros/s/SUA_NOVA_URL_AQUI/exec";
+// COLE AQUI A URL DO SEU GOOGLE APPS SCRIPT
+// (a URL que termina em /exec, copiada depois de implantar como "App da Web")
+export const API_URL = "https://script.google.com/macros/s/AKfycbwktsvyjnXYfJyMIMLJ74xVKaGApZg-1QkBvM0D3iqJ0UFifoEF36O1LskTJYFB-sw8/exec";
