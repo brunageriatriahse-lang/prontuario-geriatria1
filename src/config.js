d096f139c6eb995ab508a45450527b8e0d583d91
brunageriatriahse-lang <brunageriatriahse@gmail.com>
2026-07-08 09:49:32 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbwJhhXsNwCz2ZlwpTCaK09vDcE6LOAyPj4mHVx6y650LbCVjhHxfwsatqH_oFVhlOIb/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwJhhXsNwCz2ZlwpTCaK09vDcE6LOAyPj4mHVx6y650LbCVjhHxfwsatqH_oFVhlOIb/exec
+";
